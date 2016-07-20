Fixed all bugs relating to the derivatives of the mass function in the NM_model. This resulted in a working linear model.

NM_model/NM_model.py
@@ -1,10 +1,12 @@
 import cosmocalc as cc
 from scipy import special
+from scipy.special import gamma,digamma
 from scipy import integrate
 from scipy.interpolate import InterpolatedUnivariateSpline as IUS
 import numpy as np
 
 TOL = 1e-3
+DELTA = 1e-6
 
 class MF_model(object):
     """
@@ -52,27 +54,27 @@ class MF_model(object):
         return
 
     def B_coeff(self,d,e,f,g):
-        return 2.0/(e**d * g**(-d/2.)*special.gamma(d/2.) + g**(-f/2.)*special.gamma(f/2.))
+        return 2.0/(e**d * g**(-d/2.)*gamma(d/2.) + g**(-f/2.)*gamma(f/2.))
 
     def dB_df(self,d,e,f,g):
         B = self.B_coeff(d,e,f,g)
-        return B*B/2*(g**(-f/2.)*np.log(g)*special.gamma(f/2.)-g**(-f/2.)*special.gamma(f/2.)*special.digamma(f/2.))
+        return B*B/4.*g**(-f/2.)*gamma(f/2.)*(np.log(g) - digamma(f/2.))
 
     def dB_dg(self,d,e,f,g):
         B = self.B_coeff(d,e,f,g)
-        return B*B/2.*(d*e**d*g**(-d/2.-1)*special.gamma(d/2.) + f*g**(-f/2.-1)*special.gamma(f/2.))
+        return B*B/4.*(d*e**d*g**(-d/2.)/g*gamma(d/2.) + f*g**(-f/2.)/g*gamma(f/2.))
 
     def dg_df(self,sigma,params):
         d,e,f,g = params
         dBdf = self.dB_df(d,e,f,g)
         B = self.B_coeff(d,e,f,g)
-        return dBdf*((sigma/e)**-d+sigma**-f)*np.exp(-g/sigma**2) -B*sigma**-f*np.log(sigma)
+        return dBdf*((sigma/e)**-d+sigma**-f)*np.exp(-g/sigma**2) - B*sigma**-f*np.log(sigma)*np.exp(-g/sigma**2)
 
     def dg_dg(self,sigma,params):
         d,e,f,g = params
         dBdg = self.dB_dg(d,e,f,g)
-        g = self.calc_g(sigma,params)
-        return -g/sigma**2 + dBdg*np.exp(-g/sigma**2)*((sigma/e)**-d + sigma**-f)
+        g_sigma = self.calc_g(sigma,params)
+        return dBdg*np.exp(-g/sigma**2)*((sigma/e)**-d + sigma**-f) - g_sigma/sigma**2
         
     def calc_g(self,sigma,params):
         d,e,f,g = params
@@ -80,40 +82,61 @@ class MF_model(object):
 
     def ddf_dndM_at_M(self,lM,params):
         rhom,dln_sig_inv_dM_spline = self.rhom,self.deriv_spline
-        #M = np.exp(lM)
-        M = 10**lM
+        M = np.exp(lM)
         dgdf = self.dg_df(cc.sigmaMtophat(M,self.scale_factor),params)
-        return dgdf * self.rhom*dln_sig_inv_dM_spline(M) #*M/M #log integral
+        return dgdf*rhom*dln_sig_inv_dM_spline(M) #*M/M #log integral
 
     def ddg_dndM_at_M(self,lM,params):
         rhom,dln_sig_inv_dM_spline = self.rhom,self.deriv_spline
-        #M = np.exp(lM)
-        M = 10**lM
+        M = np.exp(lM)
         dgdg = self.dg_dg(cc.sigmaMtophat(M,self.scale_factor),params)
-        return dgdg * self.rhom*dln_sig_inv_dM_spline(M) #*M/M #log integral
+        return dgdg*rhom*dln_sig_inv_dM_spline(M) #*M/M #log integral
 
     def dndM_at_M(self,lM,params):
         rhom,dln_sig_inv_dM_spline = self.rhom,self.deriv_spline
         M = np.exp(lM)
         g_sigma = self.calc_g(cc.sigmaMtophat(M,self.scale_factor),params)
-        return g_sigma * self.rhom*dln_sig_inv_dM_spline(M) #*M/M #log integral
+        return g_sigma * rhom*dln_sig_inv_dM_spline(M) #*M/M #log integral
+    
+    def var_MF_model_in_bin(self,lMlow,lMhigh,params,variances,cov_fg = 0):
+        dNdf = integrate.quad(self.ddf_dndM_at_M,lMlow,lMhigh,args=(params))[0]*self.volume
+        dNdg = integrate.quad(self.ddg_dndM_at_M,lMlow,lMhigh,args=(params))[0]*self.volume
+        
+        #fparams1 = np.copy(params)
+        #df = params[2]*DELTA
+        #fparams1[2] += df/2.
+        #fparams2 = np.copy(params)
+        #fparams2[2] -= df/2.
+        #dNdf = (integrate.quad(self.dndM_at_M,lMlow,lMhigh,args=(fparams1))[0] - integrate.quad(self.dndM_at_M,lMlow,lMhigh,args=(fparams2))[0])/df*self.volume
 
-    def var_MF_model_in_bin(self,lMlow,lMhigh,params,variances):
-        dNdf = integrate.quad(self.ddf_dndM_at_M,lMlow,lMhigh,args=(params),\
-                                  epsabs=TOL,epsrel=TOL/10.)[0]*self.volume
-        dNdg = integrate.quad(self.ddg_dndM_at_M,lMlow,lMhigh,args=(params),\
-                                  epsabs=TOL,epsrel=TOL/10.)[0]*self.volume
-        return dNdf**2*variances[0] + dNdg**2*variances[1]
+        #gparams1 = np.copy(params)
+        #dg = params[3]*DELTA
+        #gparams1[3] += dg/2.
+        #gparams2 = np.copy(params)
+        #gparams2[3] -= dg/2.
+        #dNdg = (integrate.quad(self.dndM_at_M,lMlow,lMhigh,args=(gparams1))[0] - integrate.quad(self.dndM_at_M,lMlow,lMhigh,args=(gparams2))[0])/dg*self.volume
+
+        #print ""
+        #print dNdf, variances[0], dNdf**2*variances[0]
+        #print dNdg, variances[1], dNdg**2*variances[1]
+        #print dNdf*dNdg, cov_fg, 2*dNdf*dNdg*cov_fg
+        #print dNdf**2*variances[0] + dNdg**2*variances[1] + 2*dNdf*dNdg*cov_fg
+        
+        return dNdf**2*variances[0] + dNdg**2*variances[1] + 2*dNdf*dNdg*cov_fg
 
     def MF_model_in_bin(self,lMlow,lMhigh,params):
-        return integrate.quad(self.dndM_at_M,lMlow,lMhigh,args=(params),epsabs=TOL,epsrel=TOL/10.)[0]*self.volume
+        return integrate.quad(self.dndM_at_M,lMlow,lMhigh,args=(params))[0]*self.volume
 
     """
     The veriable 'variances' contains the
     variance in the paramters, namely f and g.
+
+    The covariance between f and g is contained in the cov_fg term,
+    which may or may not be passed in and if not is set to 0.
     """
-    def var_MF_model_all_bins(self,lM_bins,params,variances):
-        return np.array([self.var_MF_model_in_bin(lMlow,lMhigh,params,variances) for lMlow,lMhigh in lM_bins])
+    def var_MF_model_all_bins(self,lM_bins,params,variances,cov_fg = 0):
+        lM_bins_natural = np.log(10**lM_bins)
+        return np.array([self.var_MF_model_in_bin(lMlow,lMhigh,params,variances,cov_fg) for lMlow,lMhigh in lM_bins_natural])
 
     def MF_model_all_bins(self,lM_bins,params,redshift):
         if not (redshift == self.redshift):

linear_emulator/fit_lines.py
@@ -24,10 +24,12 @@ f0_path = "linear_fits/f0.txt"
 f1_path = "linear_fits/f1.txt"
 f0_var_path = "linear_fits/f0_var.txt"
 f1_var_path = "linear_fits/f1_var.txt"
+f0f1_covar_path = "linear_fits/f0f1_covar.txt"
 f0 = []
 f0_var = []
 f1 = []
 f1_var = []
+f0f1_covar = []
 for i in xrange(0,N_cosmos):
     #Loop over redshifts to assemble the correct vector
     f_i = f_all[i*N_reds:i*N_reds+10]
@@ -49,6 +51,7 @@ for i in xrange(0,N_cosmos):
     f0_var.append(cout[0,0])
     f1.append(m)
     f1_var.append(cout[1,1])
+    f0f1_covar.append(cout[0,1])
     if make_f_plots:
         domain = np.linspace(min(scale_factors),max(scale_factors),100)
         model = b+m*(pivot-domain)
@@ -59,20 +62,24 @@ f0 = np.array(f0)
 f1 = np.array(f1)
 f0_var = np.array(f0_var)
 f1_var = np.array(f1_var)
+f0f1_covar = np.array(f0f1_covar)
 
 np.savetxt(f0_path,f0)
 np.savetxt(f1_path,f1)
 np.savetxt(f0_var_path,f0_var)
 np.savetxt(f1_var_path,f1_var)
+np.savetxt(f0f1_covar_path,f0f1_covar)
 
 g0_path = "linear_fits/g0.txt"
 g1_path = "linear_fits/g1.txt"
 g0_var_path = "linear_fits/g0_var.txt"
 g1_var_path = "linear_fits/g1_var.txt"
+g0g1_covar_path = "linear_fits/g0g1_covar.txt"
 g0 = []
 g0_var = []
 g1 = []
 g1_var = []
+g0g1_covar = []
 for i in xrange(0,N_cosmos):
     #Loop over redshifts to assemble the correct vector
     g_i = g_all[i*N_reds:i*N_reds+10]
@@ -94,6 +101,7 @@ for i in xrange(0,N_cosmos):
     g0_var.append(cout[0,0])
     g1.append(m)
     g1_var.append(cout[1,1])
+    g0g1_covar.append(cout[0,1])
     if make_g_plots:
         domain = np.linspace(min(scale_factors),max(scale_factors),100)
         model = b+m*(pivot-domain)
@@ -104,11 +112,12 @@ g0 = np.array(g0)
 g1 = np.array(g1)
 g0_var = np.array(g0_var)
 g1_var = np.array(g1_var)
-
+g0g1_covar = np.array(g0g1_covar)
 np.savetxt(g0_path,g0)
 np.savetxt(g1_path,g1)
 np.savetxt(g0_var_path,g0_var)
 np.savetxt(g1_var_path,g1_var)
+np.savetxt(g0g1_covar_path,g0g1_covar)
 
 
     

linear_emulator/leave_one_out_emulation.py
@@ -3,6 +3,8 @@ This builds the leave-one-out emulators.
 """
 import numpy as np
 import sys
+import matplotlib as m
+import matplotlib.pyplot as plt
 sys.path.insert(0,"../NM_model/")
 sys.path.insert(0,"../Emulator/")
 sys.path.insert(0,"../visualization/")
@@ -21,7 +23,7 @@ data_base = "/home/tmcclintock/Desktop/Mass_Function_Data/"
 data_path = data_base+"full_mf_data/Box%03d_full/Box%03d_full_Z%d.txt"
 cov_path = data_base+"/covariances/Box%03d_cov/Box%03d_cov_Z%d.txt"
 
-build_emulators = True
+build_emulators = False
 examine_emulators = True
 
 path_to_cosmos = "../cosmology_files/building_cosmos_no_z.txt"
@@ -47,10 +49,13 @@ f0_vars = np.genfromtxt("linear_fits/f0_var.txt")
 g0_vars = np.genfromtxt("linear_fits/g0_var.txt")
 f1_vars = np.genfromtxt("linear_fits/f1_var.txt")
 g1_vars = np.genfromtxt("linear_fits/g1_var.txt")
+f0f1_covars = np.genfromtxt("linear_fits/f0f1_covar.txt")
+g0g1_covars = np.genfromtxt("linear_fits/g0g1_covar.txt")
 f0_err = np.sqrt(f0_vars)
 f1_err = np.sqrt(f1_vars)
 g0_err = np.sqrt(g0_vars)
 g1_err = np.sqrt(g1_vars)
+
 
 """
 Loop over each cosmology and
@@ -58,7 +63,6 @@ build an emulator without that cosmology included.
 
 Then save the emulator.
 """
-
 
 for i in xrange(0,Ncosmos):
     if build_emulators:
@@ -103,7 +107,7 @@ for i in xrange(0,Ncosmos):
         print "\t%s saved"%g1_name%(i)
 
     if examine_emulators:
-        print "Examining"
+        print "Examining %d"%i
         cosmo_predicted = cosmos[:,i]
         f0_real = f0_means[i]
         g0_real = g0_means[i]
@@ -122,12 +126,11 @@ for i in xrange(0,Ncosmos):
         g0_test,g0_var = g0_emu.predict_one_point(cosmo_predicted)
         f1_test,f1_var = f1_emu.predict_one_point(cosmo_predicted)
         g1_test,g1_var = g1_emu.predict_one_point(cosmo_predicted)
-        print "f0real = %f   f0 = %f +- %f"%(f0_real,f0_test,np.sqrt(f0_var))
-        print "g0real = %f   g0 = %f +- %f"%(g0_real,g0_test,np.sqrt(g0_var))
-        print "f1real = %f   f1 = %f +- %f"%(f1_real,f1_test,np.sqrt(f1_var))
-        print "g1real = %f   g1 = %f +- %f"%(g1_real,g1_test,np.sqrt(g1_var))
+        #print "f0real = %f   f0 = %f +- %f"%(f0_real,f0_test,np.sqrt(f0_var))
+        #print "g0real = %f   g0 = %f +- %f"%(g0_real,g0_test,np.sqrt(g0_var))
+        #print "f1real = %f   f1 = %f +- %f"%(f1_real,f1_test,np.sqrt(f1_var))
+        #print "g1real = %f   g1 = %f +- %f"%(g1_real,g1_test,np.sqrt(g1_var))
 
-        
         #Build a cosmo_dict
         h = cosmo_predicted[5]/100.
         Om = (cosmo_predicted[0]+cosmo_predicted[1])/h**2
@@ -138,12 +141,16 @@ for i in xrange(0,Ncosmos):
         cosmo_dict = {"om":Om,"ob":Ob,"ol":1-Om,\
                           "ok":0.0,"h":h,"s8":sigma8,\
                           "ns":ns,"w0":w0,"wa":0.0}
+
+        #Read in the hyperparameter covariances
+        param_cov = np.loadtxt("../building_data/tinker_hyperparam_covariances.txt")
+
         for z_index in xrange(0,Nreds):
-            print "\tExaminging at Z%d"%z_index
+            print "\tExamining at Z%d"%z_index
             redshift = redshifts[z_index]
             sf = 1./(1.+redshift)
             f_test = f0_test + (pivot-sf)*f1_test
-            f_var = f0_var + (pivot-sf)**2*f1_var
+            f_var = f0_var + (pivot-sf)**2*f1_var 
             g_test = g0_test + (pivot-sf)*g1_test
             g_var = g0_var + (pivot-sf)**2*g1_var
 
@@ -170,13 +177,13 @@ for i in xrange(0,Ncosmos):
             #Evaluate the model
             best_model = [1.97,1.0,f_test,g_test]
             NM_best = NM_model_obj.MF_model_all_bins(lM_bins,best_model,redshift)
-            NM_var = NM_model_obj.var_MF_model_all_bins(lM_bins,best_model,[f_var,g_var])
+            cov_fg = param_cov[0,2]+(pivot-sf)*(param_cov[1,2]+param_cov[0,3])+(pivot-sf)**2*param_cov[1,3]
+            NM_var = NM_model_obj.var_MF_model_all_bins(lM_bins,best_model,[f_var,g_var])#,cov_fg)
             NM_best_err = np.sqrt(NM_var)
             
             #for j in range(len(lM)):
             #    print lM_bins[j],"%e +- %e    %e+-%e"%(NM_data[j],NM_err[j],NM_best[j],NM_best_err[j])
 
-            import visualize
             #visualize.NM_plot(lM,NM_data,NM_err,lM,NM_best)
             title = "Box%03d left out for z=%f"%(i,redshift)
             savepath = "plots/NM_plots/NM_LOO_emulated_Box%03d_Z%d.png"%(i,z_index)