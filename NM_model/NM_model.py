Fixed the covariance matrix finally. Added chi2 analysis to the linear emulator.

NM_model/NM_model.py
@@ -98,45 +98,65 @@ class MF_model(object):
         g_sigma = self.calc_g(cc.sigmaMtophat(M,self.scale_factor),params)
         return g_sigma * rhom*dln_sig_inv_dM_spline(M) #*M/M #log integral
     
-    def var_MF_model_in_bin(self,lMlow,lMhigh,params,variances,cov_fg = 0):
-        dNdf = integrate.quad(self.ddf_dndM_at_M,lMlow,lMhigh,args=(params))[0]*self.volume
-        dNdg = integrate.quad(self.ddg_dndM_at_M,lMlow,lMhigh,args=(params))[0]*self.volume
-        
-        #fparams1 = np.copy(params)
-        #df = params[2]*DELTA
-        #fparams1[2] += df/2.
-        #fparams2 = np.copy(params)
-        #fparams2[2] -= df/2.
-        #dNdf = (integrate.quad(self.dndM_at_M,lMlow,lMhigh,args=(fparams1))[0] - integrate.quad(self.dndM_at_M,lMlow,lMhigh,args=(fparams2))[0])/df*self.volume
+    def var_MF_model_in_bin(self,lMlow,lMhigh,params,fg_variances,cov_fg = 0):
+        volume = self.volume
+        dNdf = integrate.quad(self.ddf_dndM_at_M,lMlow,lMhigh,args=(params))[0]*volume
+        dNdg = integrate.quad(self.ddg_dndM_at_M,lMlow,lMhigh,args=(params))[0]*volume
 
-        #gparams1 = np.copy(params)
-        #dg = params[3]*DELTA
-        #gparams1[3] += dg/2.
-        #gparams2 = np.copy(params)
-        #gparams2[3] -= dg/2.
-        #dNdg = (integrate.quad(self.dndM_at_M,lMlow,lMhigh,args=(gparams1))[0] - integrate.quad(self.dndM_at_M,lMlow,lMhigh,args=(gparams2))[0])/dg*self.volume
-
+        #delf = params[2]*DELTA
+        #delg = params[3]*DELTA
+        #paramsf1 = np.copy(params)
+        #paramsf2 = np.copy(params)
+        #paramsf1[2] = paramsf1[2]+delf/2.
+        #paramsf2[2] = paramsf2[2]-delf/2.
+        #paramsg1 = np.copy(params)
+        #paramsg2 = np.copy(params)
+        #paramsg1[3] = paramsg1[3]+delg/2.
+        #paramsg2[3] = paramsg2[3]-delg/2.
+        #dNdf = (integrate.quad(self.dndM_at_M,lMlow,lMhigh,args=(paramsf1))[0]-integrate.quad(self.dndM_at_M,lMlow,lMhigh,args=(paramsf2))[0])*volume/delf
+        #dNdg = (integrate.quad(self.dndM_at_M,lMlow,lMhigh,args=(paramsg1))[0]-integrate.quad(self.dndM_at_M,lMlow,lMhigh,args=(paramsg2))[0])*volume/delg
         #print ""
-        #print dNdf, variances[0], dNdf**2*variances[0]
-        #print dNdg, variances[1], dNdg**2*variances[1]
+        #print dNdf, fg_variances[0], dNdf**2*fg_variances[0]
+        #print dNdg, fg_variances[1], dNdf**2*fg_variances[1]
         #print dNdf*dNdg, cov_fg, 2*dNdf*dNdg*cov_fg
-        #print dNdf**2*variances[0] + dNdg**2*variances[1] + 2*dNdf*dNdg*cov_fg
+        #print dNdf**2*fg_variances[0] + dNdg**2*fg_variances[1] + 2*dNdf*dNdg*cov_fg
+        #print ""
         
-        return dNdf**2*variances[0] + dNdg**2*variances[1] + 2*dNdf*dNdg*cov_fg
+        return dNdf**2*fg_variances[0] + dNdg**2*fg_variances[1] + 2*dNdf*dNdg*cov_fg
 
     def MF_model_in_bin(self,lMlow,lMhigh,params):
         return integrate.quad(self.dndM_at_M,lMlow,lMhigh,args=(params))[0]*self.volume
 
     """
-    The veriable 'variances' contains the
+    The veriable 'fg_variances' contains the
     variance in the paramters, namely f and g.
 
     The covariance between f and g is contained in the cov_fg term,
     which may or may not be passed in and if not is set to 0.
     """
-    def var_MF_model_all_bins(self,lM_bins,params,variances,cov_fg = 0):
+    def var_MF_model_all_bins(self,lM_bins,params,fg_variances,cov_fg = 0):
         lM_bins_natural = np.log(10**lM_bins)
-        return np.array([self.var_MF_model_in_bin(lMlow,lMhigh,params,variances,cov_fg) for lMlow,lMhigh in lM_bins_natural])
+        return np.array([self.var_MF_model_in_bin(lMlow,lMhigh,params,fg_variances,cov_fg) for lMlow,lMhigh in lM_bins_natural])
+
+    def covariance_MF(self,lM_bins,params,fg_variances,cov_fg = 0):
+        lM_bins_natural = np.log(10**lM_bins)
+        dNdf_all = []
+        dNdg_all = []
+        for lMlow,lMhigh in lM_bins_natural:
+            dNdf_all.append(integrate.quad(self.ddf_dndM_at_M,lMlow,lMhigh,args=(params))[0]*self.volume)
+            dNdg_all.append(integrate.quad(self.ddg_dndM_at_M,lMlow,lMhigh,args=(params))[0]*self.volume)
+        dNdf_all = np.array(dNdf_all)
+        dNdg_all = np.array(dNdg_all)
+        
+        cov_MF = np.zeros((len(lM_bins),len(lM_bins)))
+        f_var = fg_variances[0]
+        g_var = fg_variances[1]
+        for i in range(len(lM_bins)):
+            for j in range(len(lM_bins)):
+                cov_MF[i,j] = dNdf_all[i]*dNdf_all[j]*f_var + dNdg_all[i]*dNdg_all[j]*g_var + cov_fg*(dNdf_all[i]*dNdg_all[j]+dNdf_all[j]*dNdg_all[i])
+                continue # end j
+            continue # end i
+        return cov_MF
 
     def MF_model_all_bins(self,lM_bins,params,redshift):
         if not (redshift == self.redshift):

linear_emulator/construct_covariance_matrix.py
@@ -6,10 +6,13 @@ Calculate (F - F_emu) where F is f0, f1, g0, g1
 for each cosmology using its LOO emulator.
 """
 
+showplots = True
+
 import numpy as np
 import sys
 import matplotlib as m
 import matplotlib.pyplot as plt
+#plt.rc('text',usetex=True, fontsize=20)
 sys.path.insert(0,"../Emulator/")
 import Emulator
 
@@ -57,82 +60,104 @@ f1_err = np.sqrt(f1_vars)
 g0_err = np.sqrt(g0_vars)
 g1_err = np.sqrt(g1_vars)
 
-cov = np.zeros((4,4))
 #Observed paramters f0, f1, g0, g1
 obs = np.zeros((4,Ncosmos))
 #Model aka emulated parameters
 model = np.zeros((4,Ncosmos))
 
+#First loop over each cosmology and add its LOO emulator to a list
+f0_list = []
+f1_list = []
+g0_list = []
+g1_list = []
+for i in xrange(0,Ncosmos):
+    space = np.ones_like(cosmos[:,0]) #A filler array
+    f0_list.append(Emulator.Emulator(space,space,space))
+    f1_list.append(Emulator.Emulator(space,space,space))
+    g0_list.append(Emulator.Emulator(space,space,space))
+    g1_list.append(Emulator.Emulator(space,space,space))
+    f0_list[i].load("saved_emulators/leave_one_out_emulators/%s"%f0_name%(i))
+    g0_list[i].load("saved_emulators/leave_one_out_emulators/%s"%g0_name%(i))
+    f1_list[i].load("saved_emulators/leave_one_out_emulators/%s"%f1_name%(i))
+    g1_list[i].load("saved_emulators/leave_one_out_emulators/%s"%g1_name%(i))
+    
 for i in xrange(0,Ncosmos):
     cosmo_predicted = cosmos[:,i]
-    #f0_real = f0_means[i]
-    #g0_real = g0_means[i]
-    #f1_real = f1_means[i]
-    #g1_real = g1_means[i]
-
-    f0_real,f0_real_var = f0_full_emu.predict_one_point(cosmo_predicted)
-    f1_real,f1_real_var = f1_full_emu.predict_one_point(cosmo_predicted)
-    g0_real,g0_real_var = g0_full_emu.predict_one_point(cosmo_predicted)
-    g1_real,g1_real_var = g1_full_emu.predict_one_point(cosmo_predicted)
-
-    space = np.ones_like(cosmos[:,0]) #A filler array
-    f0_emu = Emulator.Emulator(space,space,space)
-    g0_emu = Emulator.Emulator(space,space,space)
-    f1_emu = Emulator.Emulator(space,space,space)
-    g1_emu = Emulator.Emulator(space,space,space)
-    f0_emu.load("saved_emulators/leave_one_out_emulators/%s"%f0_name%(i))
-    g0_emu.load("saved_emulators/leave_one_out_emulators/%s"%g0_name%(i))
-    f1_emu.load("saved_emulators/leave_one_out_emulators/%s"%f1_name%(i))
-    g1_emu.load("saved_emulators/leave_one_out_emulators/%s"%g1_name%(i))
-    f0_test,f0_var = f0_emu.predict_one_point(cosmo_predicted)
-    g0_test,g0_var = g0_emu.predict_one_point(cosmo_predicted)
-    f1_test,f1_var = f1_emu.predict_one_point(cosmo_predicted)
-    g1_test,g1_var = g1_emu.predict_one_point(cosmo_predicted)
-    
-    print "i = %d"%i
-    print "f0real = %f +- %f   f0 = %f +- %f"%(f0_real,np.sqrt(f0_real_var),f0_test,np.sqrt(f0_var))
-    print "g0real = %f +- %f   g0 = %f +- %f"%(g0_real,np.sqrt(g0_real_var),g0_test,np.sqrt(g0_var))
-    print "f1real = %f +- %f   f1 = %f +- %f"%(f1_real,np.sqrt(f1_real_var),f1_test,np.sqrt(f1_var))
-    print "g1real = %f +- %f   g1 = %f +- %f"%(g1_real,np.sqrt(g1_real_var),g1_test,np.sqrt(g1_var))
-    print ""
-    
-    obs[0,i] = f0_real
-    obs[1,i] = f1_real
-    obs[2,i] = g0_real
-    obs[3,i] = g1_real
+    f0_test,f0_var = f0_list[i].predict_one_point(cosmo_predicted)
+    g0_test,g0_var = g0_list[i].predict_one_point(cosmo_predicted)
+    f1_test,f1_var = f1_list[i].predict_one_point(cosmo_predicted)
+    g1_test,g1_var = g1_list[i].predict_one_point(cosmo_predicted)
     model[0,i] = f0_test
     model[1,i] = f1_test
     model[2,i] = g0_test
     model[3,i] = g1_test
+
+    #obs[0,i] = f0_means[i]
+    #obs[1,i] = f1_means[i]
+    #obs[2,i] = g0_means[i]
+    #obs[3,i] = g1_means[i]
+    #f0_full,f0_full_var = f0_full_emu.predict_one_point(cosmo_predicted)
+    #f1_full,f1_full_var = f1_full_emu.predict_one_point(cosmo_predicted)
+    #g0_full,g0_full_var = g0_full_emu.predict_one_point(cosmo_predicted)
+    #g1_full,g1_full_var = g1_full_emu.predict_one_point(cosmo_predicted)
+    f0_mean = f1_mean = g0_mean = g1_mean = 0
+    for j in xrange(0,Ncosmos):
+        f0_test,f0_var = f0_list[j].predict_one_point(cosmo_predicted)
+        g0_test,g0_var = g0_list[j].predict_one_point(cosmo_predicted)
+        f1_test,f1_var = f1_list[j].predict_one_point(cosmo_predicted)
+        g1_test,g1_var = g1_list[j].predict_one_point(cosmo_predicted)
+        f0_mean += f0_test/Ncosmos
+        f1_mean += f1_test/Ncosmos
+        g0_mean += g0_test/Ncosmos
+        g1_mean += g1_test/Ncosmos
+        continue #end j
+    obs[0,i] = f0_mean
+    obs[1,i] = f1_mean
+    obs[2,i] = g0_mean
+    obs[3,i] = g1_mean
+    continue #end i
+
+indices = np.arange(Ncosmos)
 diff = obs - model
-pf = (Ncosmos-1.0)/Ncosmos #prefactor
+names = ["f0","f1","g0","g1"]
 for i in range(4):
-    print diff[i]/model[i]
+    plt.plot(indices,diff[i]/model[i],marker='o',ls='',label=names[i])
+plt.legend()
+plt.xlabel("Simulation number")
+plt.ylabel("% diff")
+if showplots:
+    plt.show()
+plt.close()
+
+pf = (Ncosmos-1.0)/Ncosmos #prefactor
+cov = np.zeros((4,4))
+print diff.shape
+for i in range(4):
     for j in range(4):
-        cov[i,j] = pf * np.sum(diff[i]*diff[j])
+        cov[i,j] = pf * np.sum(diff[i]*diff[j])/Ncosmos
 print cov
 np.savetxt("../building_data/tinker_hyperparam_covariances.txt",cov)
-
-inds = np.arange(Ncosmos)
-plt.plot(inds,diff[0])
-plt.plot(inds,diff[1])
-plt.plot(inds,diff[2])
-plt.plot(inds,diff[3])
-plt.show()
 
 corr = np.ones_like(cov)
 for i in xrange(0,4):
     for j in xrange(0,4):
         corr[i,j] = cov[i,j]/np.sqrt(cov[i,i]*cov[j,j])
-print corr
+#print corr
 
 plt.pcolor(corr,vmin=-1.0,vmax = 1.0,cmap ='RdBu_r')
-plt.show()
-    
+if showplots:
+    plt.show()
+plt.close()    
+
 #Covariance goes f0, f1, g0, g1
 def cov_fg(a,cov):
     k = 1./2.-a
     return cov[0,2]+k*(cov[0,3]+cov[1,2])+k*k*cov[1,3]
 domain = np.arange(0,1,0.02)
 plt.plot(domain,cov_fg(domain,cov))
-plt.show()
+plt.ylabel(r"$C(f,g)$")
+plt.xlabel(r"$a$")
+plt.subplots_adjust(left=0.2,bottom=0.15)
+if showplots:
+    plt.show()
+plt.close()

linear_emulator/leave_one_out_emulation.py
@@ -25,6 +25,11 @@ cov_path = data_base+"/covariances/Box%03d_cov/Box%03d_cov_Z%d.txt"
 
 build_emulators = False
 examine_emulators = True
+chi2_test = True
+visualize_curves = False
+
+chi2_array = np.zeros((Ncosmos,Nreds))
+dof_array = np.zeros((Ncosmos,Nreds))
 
 path_to_cosmos = "../cosmology_files/building_cosmos_no_z.txt"
 cosmos = np.genfromtxt(path_to_cosmos).T
@@ -177,16 +182,40 @@ for i in xrange(0,Ncosmos):
             #Evaluate the model
             best_model = [1.97,1.0,f_test,g_test]
             NM_best = NM_model_obj.MF_model_all_bins(lM_bins,best_model,redshift)
-            cov_fg = param_cov[0,2]+(pivot-sf)*(param_cov[1,2]+param_cov[0,3])+(pivot-sf)**2*param_cov[1,3]
-            NM_var = NM_model_obj.var_MF_model_all_bins(lM_bins,best_model,[f_var,g_var])#,cov_fg)
-            NM_best_err = np.sqrt(NM_var)
-            
-            #for j in range(len(lM)):
-            #    print lM_bins[j],"%e +- %e    %e+-%e"%(NM_data[j],NM_err[j],NM_best[j],NM_best_err[j])
 
-            #visualize.NM_plot(lM,NM_data,NM_err,lM,NM_best)
+            cov_fg = param_cov[0,2] + (pivot-sf)*(param_cov[1,2]+param_cov[0,3]) + (pivot-sf)**2*param_cov[1,3]
+            cov_ff = param_cov[0,0] + (pivot-sf)*param_cov[0,1] + (pivot-sf)**2*param_cov[1,1]
+            cov_gg = param_cov[2,2] + (pivot-sf)*param_cov[2,3] + (pivot-sf)**2*param_cov[3,3]
+            #print param_cov
+            #print cov_fg,cov_ff,cov_gg
+
+            NM_var = NM_model_obj.var_MF_model_all_bins(lM_bins,best_model,[cov_ff,cov_gg],cov_fg)
+            NM_best_err = np.sqrt(NM_var)
+                        
             title = "Box%03d left out for z=%f"%(i,redshift)
-            savepath = "plots/NM_plots/NM_LOO_emulated_Box%03d_Z%d.png"%(i,z_index)
-            sigma_savepath = "plots/gsigma_plots/gsigma_LOO_emulated_Box%03d_Z%d.png"%(i,z_index)
-            visualize.NM_emulated(lM,NM_data,NM_err,lM,NM_best,NM_best_err,title,savepath)
-            visualize.g_sigma_emulated(NM_model_obj,redshift,volume,cosmo_dict,lM,lM_bins,NM_data,NM_err,best_model,[f_var,g_var],title,sigma_savepath)
+
+            if chi2_test:
+                chi2_cosmo = 0
+                cov_emu = NM_model_obj.covariance_MF(lM_bins,best_model,[cov_ff,cov_gg],cov_fg)
+                #print cov.shape, cov_emu.shape
+                #print cov.shape, cov_emu.shape, NM_data.shape, NM_best.shape
+                cov_full = cov+cov_emu
+                icov = np.linalg.inv(cov_full)
+                diff = NM_data - NM_best
+                #print icov.shape, diff.shape
+                for j in range(len(icov)):
+                    for k in range(len(icov[j])):
+                        chi2_cosmo += diff[j] * icov[j,k] * diff[k]
+                #print "\tchi2 = %e"%chi2_cosmo
+                chi2_array[i,z_index] = chi2_cosmo
+                dof_array[i,z_index] = len(NM_data)
+
+            if visualize_curves:
+                savepath = "plots/NM_plots/NM_LOO_emulated_Box%03d_Z%d.png"%(i,z_index)
+                sigma_savepath = "plots/gsigma_plots/gsigma_LOO_emulated_Box%03d_Z%d.png"%(i,z_index)
+                visualize.NM_emulated(lM,NM_data,NM_err,lM,NM_best,NM_best_err,title,savepath)
+                #visualize.g_sigma_emulated(NM_model_obj,redshift,volume,cosmo_dict,lM,lM_bins,NM_data,NM_err,best_model,[f_var,g_var],title,sigma_savepath)
+
+if chi2_test and examine_emulators:
+    np.savetxt("txt_files/LOO_chi2.txt",chi2_array)
+    np.savetxt("txt_files/LOO_dof.txt",dof_array)